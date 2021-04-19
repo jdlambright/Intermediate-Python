3a6b2198b937d5cf187d9f23e0d0a49207e5c441
jdlambright <64182772+jdlambright@users.noreply.github.com>
2021-04-19 15:50:18 -0400

    went back and practiced dictionaries

---
+++ 1 Previous Notes/notes/dictionary_practice.py
@@ -0,0 +1,135 @@
+##Python Dictionaries this note shows how to use dictionaries
+
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.",
+  "Function": "A piece of code that you can easily call over and over again.",
+}
+
+#Retrieving items from dictionary.
+# print(programming_dictionary["Function"])
+
+#Adding new items to dictionary.
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+#Create an empty dictionary.
+empty_dictionary = {}
+
+#Wipe an existing dictionary
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+#Edit an item in a dictionary
+programming_dictionary["Bug"] = "A moth in your computer."
+# print(programming_dictionary)
+
+#Loop through a dictionary
+# for key in programming_dictionary:
+#   print(key)
+#   print(programming_dictionary[key])
+
+#######################################
+
+#grade project
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99,
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+# Create an empty dictionary called student_grades.
+
+student_grades = {}
+
+# Write your code below to add the grades to student_grades.👇
+for student in student_scores:
+  score = student_scores[student]
+  if score > 90:
+    student_grades[student] = "A"
+
+  elif score > 80:
+    student_grades[student] = "B"
+
+  elif score > 70:
+    student_grades[student] = "C"
+
+  else:
+    student_grades[student] = "fail"
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
+
+#Nesting
+capitals = {
+  "France": "Paris",
+  "Germany": "Berlin",
+}
+
+#Nesting a List in a Dictionary
+
+travel_log = {
+  "France": ["Paris", "Lille", "Dijon"],
+  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+#Nesting Dictionary in a Dictionary
+
+travel_log = {
+  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+}
+
+#Nesting Dictionaries in Lists
+
+travel_log = [
+{
+  "country": "France",
+  "cities_visited": ["Paris", "Lille", "Dijon"],
+  "total_visits": 12,
+},
+{
+  "country": "Germany",
+  "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+  "total_visits": 5,
+},
+]
+
+
+
+
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
+
+#Write the function that will allow new countries
+#to be added to the travel_log. 👇
+
+def add_new_country(country_visited, times_visited, cities_visited):
+  new_country = {}
+  new_country["country"] = country_visited
+  new_country["visits"] = times_visited
+  new_country["cities"] = cities_visited
+  travel_log.append(new_country)
+
+
+
+
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
+
+
+
--- 34-40/39-40_Flight_Finder_Capstone/flight_data.py
+++ 34-40/39-40_Flight_Finder_Capstone/flight_data.py
@@ -2,8 +2,9 @@ import requests
 
 response = requests.get(url="https://api.sheety.co/be411e8cf62d3966a9666c5b539f30ea/flightDeals/sheet1")
 response.raise_for_status()
-data = response.json()
-print(data.text)
+data = response.text
+print(data)
+
 
 
 
